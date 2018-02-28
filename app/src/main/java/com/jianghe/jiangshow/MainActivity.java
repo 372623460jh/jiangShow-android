8c917ae4eea125a31cccf8f7b0aae035cb660c42
jianghe <573748150jh@163.com>
2018-02-28 23:35:24 +0800

    修改闪屏页，取消白屏

--- app/src/main/java/com/jianghe/jiangshow/MainActivity.java
+++ app/src/main/java/com/jianghe/jiangshow/MainActivity.java
@@ -3,7 +3,6 @@ package com.jianghe.jiangshow;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Window;
 import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
